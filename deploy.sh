35d33b39de7118d5773acc2006f17ac723d02c13
Prabir Ghosh <mymail.prabir@gmail.com>
2019-01-22 13:31:30 +0530

    Update Script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:prabirsoft/prabirsoft.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:prabirsoft/cv.git master:gh-pages
+# git push -f git@github.com:prabirsoft/cv.git master:gh-pages
 
 cd -
