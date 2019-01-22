b2a9f99b0bf984fcac2130c04c23eeb4f19e4240
Prabir Ghosh <mymail.prabir@gmail.com>
2019-01-22 13:06:24 +0530

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 set -e
 
 # build
+git checkout -b 
 npm run build
 
 # navigate into the build output directory
@@ -17,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:prabirsoft/prabirsoft.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:prabirsoft/cv.git master:gh-pages
 
 cd -
